c45619fd4daa8c717c15e712963e20514d465a08
andrevinicus <102330781+andrevinicus@users.noreply.github.com>
2022-08-15 16:29:06 -0300

    Update Execos.java

--- Execos.java
+++ Execos.java
@@ -1,10 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
 package com.mycompany.execos;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -15,11 +11,20 @@ public class Execos {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-        String[] vect = sc.nextLine().split(" ");
-        int position = sc.nextInt();
-        
-        System.out.println(vect[position]);
-        
+        try {
+            String[] vect = sc.nextLine().split(" ");
+            int position = sc.nextInt();
+            System.out.println(vect[position]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid position");
+
+        }
+        catch(InputMismatchException e){
+            System.out.println("Input error");
+        }
+        System.out.println("End of Program");
+
+        sc.close();
     }
 }
+
